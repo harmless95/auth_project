8083ea5d1e5e2a78cd58eea253a32bd9aa811007
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-15 20:24:43 +0700

    create decode jwt token

--- my_project/utils/validates.py
+++ my_project/utils/validates.py
@@ -9,9 +9,9 @@ from core.config import setting
 
 def encode_jwt(
     payload: dict,
-    private_key: str = setting.auth_jwt.private_key_path.read_text(),
-    algorithm: str = setting.auth_jwt.algorithm,
-    expire_minutes: int = setting.auth_jwt.access_token_expire_minutes,
+    private_key: str,
+    algorithm: str,
+    expire_minutes: int,
     expire_timedelta: timedelta | None = None,
 ) -> str:
     to_encode = payload.copy()
@@ -31,6 +31,19 @@ def encode_jwt(
         algorithm=algorithm,
     )
     return encoded
+
+
+def decode_jwt(
+    token: str | bytes,
+    public_key: str,
+    algorithm: str,
+) -> dict:
+    decoded = jwt.decode(
+        token,
+        public_key,
+        algorithms=[algorithm],
+    )
+    return decoded
 
 
 def hash_password(password: str) -> bytes:
