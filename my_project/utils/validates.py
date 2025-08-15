bd5cabcbfbd62b0eaee822c42d2b9ba30d6f9315
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-15 15:55:03 +0700

    create generates token jwt

--- my_project/utils/validates.py
+++ my_project/utils/validates.py
@@ -1,4 +1,36 @@
+import uuid
+from datetime import timedelta, datetime, timezone
+import jwt
+
 import bcrypt
+
+from core.config import setting
+
+
+def encode_jwt(
+    payload: dict,
+    private_key: str = setting.auth_jwt.private_key_path.read_text(),
+    algorithm: str = setting.auth_jwt.algorithm,
+    expire_minutes: int = setting.auth_jwt.access_token_expire_minutes,
+    expire_timedelta: timedelta | None = None,
+) -> str:
+    to_encode = payload.copy()
+    now = datetime.now(timezone.utc)
+    if expire_timedelta:
+        expire = now + expire_timedelta
+    else:
+        expire = now + timedelta(minutes=expire_minutes)
+    to_encode.update(
+        exp=expire,
+        iat=now,
+        jti=str(uuid.uuid4()),
+    )
+    encoded = jwt.encode(
+        to_encode,
+        private_key,
+        algorithm=algorithm,
+    )
+    return encoded
 
 
 def hash_password(password: str) -> bytes:
