1b8407a86ea44731e44f24cb4e6bbfb494043fd1
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-15 15:54:33 +0700

    create validates password

--- my_project/utils/validates.py
+++ my_project/utils/validates.py
@@ -5,3 +5,14 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     pwd_bites: bytes = password.encode()
     return bcrypt.hashpw(pwd_bites, salt)
+
+
+def validates_password(
+    password: str,
+    password_hash: str,
+) -> bool:
+    password_hash = bytes.fromhex(password_hash)
+    return bcrypt.checkpw(
+        password=password.encode(),
+        hashed_password=password_hash,
+    )
