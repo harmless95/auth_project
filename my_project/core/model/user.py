f73c631781ecbf908a04676377082c67c9ff6f45
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-15 14:29:41 +0700

    password save database password_hash

--- my_project/core/model/user.py
+++ my_project/core/model/user.py
@@ -5,6 +5,8 @@ from .base import Base
 
 
 class User(Base):
-    email: Mapped[str] = mapped_column(String(length=320), unique=True, index=True, nullable=False)
-    password: Mapped[str] = mapped_column(String(1024), nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(length=320), unique=True, index=True, nullable=False
+    )
+    password_hash: Mapped[str] = mapped_column(String(1024), nullable=False)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
