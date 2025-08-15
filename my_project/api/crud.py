8dab19d585255e22dc06ba6a4990df63a8a39b6a
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-15 12:46:17 +0700

    create register user

--- my_project/api/crud.py
+++ my_project/api/crud.py
@@ -1,4 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from fastapi import status, HTTPException
 
 from core.model import User
 from core.schema.user import UserCreate
@@ -8,6 +10,14 @@ async def create_user(
     session: AsyncSession,
     data_user: UserCreate,
 ) -> User:
+    stmt = select(User).where(User.email == data_user.email)
+    result = await session.scalars(stmt)
+    user = result.first()
+    if user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"Invalid there is already a user with this: {data_user.email}",
+        )
     user = User(**data_user.model_dump())
     session.add(user)
     await session.commit()
--- my_project/api/user_api.py
+++ my_project/api/user_api.py
@@ -1,17 +1,22 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.crud import create_user
-from core.model import db_helper
+from core.model import db_helper, User
 from core.schema.user import UserCreate, UserRead
 
 router = APIRouter(prefix="/user", tags=["User"])
 
 
-@router.post("/")
+@router.post(
+    "/",
+    response_model=UserRead,
+    status_code=status.HTTP_201_CREATED,
+)
 async def register_user(
     session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
     data_user: UserCreate,
-) -> UserRead:
-    return await create_user(session=session, data_user=data_user)
+) -> User:
+    user = await create_user(session=session, data_user=data_user)
+    return user
