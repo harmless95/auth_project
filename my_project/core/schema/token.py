3edc1902db0c9f2158e09927083d8a06ad4a5251
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-18 18:06:07 +0700

    add refresh token None

--- my_project/core/schema/token.py
+++ my_project/core/schema/token.py
@@ -1,7 +1,11 @@
 from pydantic import BaseModel, ConfigDict
 
+from core.config import setting
+
+
 class TokenBase(BaseModel):
     access_token: str
-    token_type: str
+    refresh_token: str | None = None
+    token_type: str = setting.auth_jwt.type_token
 
     model_config = ConfigDict(from_attributes=True)
